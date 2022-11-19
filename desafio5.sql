66a5cf44532bf41d56341c136e3e35b9582c4615
Claudio Melgaco <claudio19a@hotmail.com>
2022-11-19 11:38:01 -0300

    req 5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT t.track_name AS 'cancao',
+COUNT (r.track_id) AS 'reproducoes'
+FROM SpotifyClone.reproduction AS r
+INNER JOIN SpotifyClone.tracks AS t ON (t.track_id = r.track_id)
+GROUP BY r.track_id
+ORDER BY reproducoes DESC, cancao ASC
+LIMIT 2;
