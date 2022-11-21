a778c5483ccf0f2282aa59d1a7f57a58c33e1055
Claudio Melgaco <claudio19a@hotmail.com>
2022-11-21 15:04:54 -0300

    req 11 de novo

--- desafio11.sql
+++ desafio11.sql
@@ -1,14 +1,14 @@
 SELECT t.track_name AS 'nome_musica',
-CASE WHEN track_name LIKE '%Bard%' THEN REPLACE(track_name, 'Bard', 'QA')
-WHEN track_name LIKE '%Amar%' THEN REPLACE(track_name, 'Amar', 'Code Review')
-WHEN track_name LIKE '%Pais%' THEN REPLACE(track_name, 'Pais', 'Pull Requests')
-WHEN track_name LIKE '%SOUL%' THEN REPLACE(track_name, 'SOUL', 'CODE')
-WHEN track_name LIKE '%SUPERSTAR%' THEN REPLACE(track_name, 'SUPERSTAR', 'SUPERDEV')
+CASE WHEN track_name LIKE '%Bard%' THEN REPLACE(t.track_name, 'Bard', 'QA')
+WHEN t.track_name LIKE '%Amar%' THEN REPLACE(t.track_name, 'Amar', 'Code Review')
+WHEN t.track_name LIKE '%Pais%' THEN REPLACE(t.track_name, 'Pais', 'Pull Requests')
+WHEN t.track_name LIKE '%SOUL%' THEN REPLACE(t.track_name, 'SOUL', 'CODE')
+WHEN t.track_name LIKE '%SUPERSTAR%' THEN REPLACE(t.track_name, 'SUPERSTAR', 'SUPERDEV')
 END AS 'novo_nome'
 FROM SpotifyClone.tracks AS t
-WHERE track_name LIKE '%Bard%'
-OR track_name LIKE '%Amar%'
-OR track_name LIKE '%Pais%'
-OR track_name LIKE '%SOUL%'
-OR track_name LIKE '%SUPERSTAR%'
+WHERE t.track_name LIKE '%Bard%'
+OR t.track_name LIKE '%Amar%'
+OR t.track_name LIKE '%Pais%'
+OR t.track_name LIKE '%SOUL%'
+OR t.track_name LIKE '%SUPERSTAR%'
 ORDER BY nome_musica DESC;
